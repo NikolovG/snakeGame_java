b72454d740d99fed4ae85b181242142b6dde98cb
Georgi Nikolov <Nikolov.georgi18@gmail.com>
2022-01-21 22:42:12 -0600

    added files

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {  //superClass = "App"
+
+    public static void main(String[] args){  // Main function
+        System.out.println("Game Started...");
+        new GameFrame();  // shortcut for GameFrame frame = new GameFrame();
+        System.out.println("Window Opened...");
+    }
+
+}
